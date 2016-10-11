dd0997e83d97ab4bef4e0254e250fa2a32d0fefb
Brandon Nicoll <bgnicoll@gmail.com>
2016-10-11 13:39:25 -0500

    Removed unneeded file

--- ButterCMS/Models/ContentFieldsResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ButterCMS.Models
-{
-    public class ContentFieldsResponse
-    {
-        public Dictionary<string, dynamic> Data { get; set; }
-    }
-}
