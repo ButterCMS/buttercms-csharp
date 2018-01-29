c6de046f18a81b6c75d91bb432627a6496abcfaf
Brandon Nicoll <bgnicoll@gmail.com>
2018-01-28 20:31:50 -0600

    Expanding .NET Framework Compatibility

---
+++ ButterCMS.Tests/Common.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ButterCMS.Tests
+{
+    public static class Common
+    {
+        private static IConfiguration Configuration { get; set; }
+        public static ButterCMSClient SetUpButterClient()
+        {
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            return new ButterCMSClient(Configuration["apiKey"]);
+        }
+    }
+}
