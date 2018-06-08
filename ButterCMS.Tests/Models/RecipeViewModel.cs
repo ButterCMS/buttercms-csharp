31c0cf8fc808a3c819bbcc9ed8de7e9c02d8f6c7
Brandon Nicoll <bgnicoll@gmail.com>
2018-06-08 14:09:39 -0500

    Tests to ensure the examples are functionally correct.

---
+++ ButterCMS.Tests/Models/RecipeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ButterCMS.Tests.Models
+{
+    public class RecipeViewModel
+    {
+        public string Category { get; set; }
+        public string RecipeName { get; set; }
+        public string MainIngredient { get; set; }
+        public double EstimatedCookingTimeInMinutes { get; set; }
+        public string IngredientList { get; set; }
+        public string Instructions { get; set; }
+    }
+}
---
+++ ButterCMS.Tests/ReadmePageTypeExamples.cs
@@ -0,0 +1,75 @@
+﻿using ButterCMS.Models;
+using ButterCMS.Tests.Models;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ButterCMS.Tests
+{
+    [TestFixture]
+    public class ReadmePageTypeExamples
+    {
+        private ButterCMSClient butterClient;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            butterClient = Common.SetUpButterClient();
+        }
+
+
+        [Test]
+        public void ReadmeExample_ListPages()
+        {
+            int page = 1;
+            int pageSize = 10;
+
+            var parameterDict = new Dictionary<string, string>()
+            {
+                {"page", page.ToString()},
+                {"page_size", pageSize.ToString()}
+            };
+
+            PagesResponse<RecipePage> recipePages = butterClient.ListPages<RecipePage>("recipe", parameterDict);
+
+            var viewModel = new RecipesViewModel();
+            viewModel.PreviousPageNumber = recipePages.Meta.PreviousPage;
+            viewModel.NextPageNumber = recipePages.Meta.NextPage;
+            viewModel.PagesCount = recipePages.Meta.Count;
+
+            viewModel.Recipes = new List<RecipeViewModel>();
+            foreach (Page<RecipePage> recipe in recipePages.Data)
+            {
+                RecipeViewModel recipeViewModel = new RecipeViewModel();
+                recipeViewModel.Category = recipe.Fields.category;
+                recipeViewModel.RecipeName = recipe.Fields.recipe_name;
+                recipeViewModel.MainIngredient = recipe.Fields.main_ingredient;
+                recipeViewModel.EstimatedCookingTimeInMinutes = recipe.Fields.estimated_cooking_time_in_minutes;
+                recipeViewModel.IngredientList = recipe.Fields.ingredient_list;
+                recipeViewModel.Instructions = recipe.Fields.instructions;
+
+                viewModel.Recipes.Add(recipeViewModel);
+            }
+
+            Assert.IsNotNull(viewModel);
+        }
+
+        [Test]
+        public void ReadmeExample_RetrievePage()
+        {
+            var slug = "chicken-soup";
+
+            PageResponse<RecipePage> recipe = butterClient.RetrievePage<RecipePage>("recipe", slug);
+
+            var viewModel = new RecipeViewModel();
+            viewModel.Category = recipe.Data.Fields.category;
+            viewModel.RecipeName = recipe.Data.Fields.recipe_name;
+            viewModel.MainIngredient = recipe.Data.Fields.main_ingredient;
+            viewModel.EstimatedCookingTimeInMinutes = recipe.Data.Fields.estimated_cooking_time_in_minutes;
+            viewModel.IngredientList = recipe.Data.Fields.ingredient_list;
+            viewModel.Instructions = recipe.Data.Fields.instructions;
+
+            Assert.IsNotNull(viewModel);
+        }
+    }
+}
