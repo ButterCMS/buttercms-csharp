6e23a521a37b13e4f6fae6dde246a4546976cbb9
Vlada Nevyhosteny <vladimir.nevyhosteny@applifting.cz>
2023-09-03 14:52:34 +0200

    fix: ListPosts tests

--- ButterCMS.Tests/ListPostsTests.cs
+++ ButterCMS.Tests/ListPostsTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ButterCMS.Tests
@@ -7,58 +10,108 @@ namespace ButterCMS.Tests
     [Category("ListPosts")]
     public class ListPostsTests
     {
-        private ButterCMSClient butterClient;
+        private ButterCMSClientWithMockedHttp butterClient;
 
-        [OneTimeSetUp]
+        [SetUp]
         public void SetUp()
         {
-            butterClient = Common.SetUpButterClient();
+            butterClient = Common.SetUpMockedButterClient();
         }
 
         [Test]
         public void ListPosts_ShouldReturnPosts()
         {
+            butterClient.MockSuccessfullPostsResponse();
+
             var posts = butterClient.ListPosts();
             Assert.IsNotNull(posts);
             CollectionAssert.IsNotEmpty(posts.Data);
+
+            var post = posts.Data.First();
+            Assert.AreEqual(PostsMocks.Post.Slug, post.Slug); 
+            Assert.AreEqual(PostsMocks.Post.Title, post.Title); 
         }
 
         [Test]
         public void ListPosts_FullParams_ShouldReturnPosts()
         {   
-            var posts = butterClient.ListPosts(page: 1, pageSize: 2, excludeBody: true, authorSlug: "api-test", categorySlug: "test-category", tagSlug: "test-tag");
+            var page = 1;
+            var pageSize = 2;
+            var excludeBody = true;
+            var authorSlug = "test-author";
+            var categorySlug = "test-category";
+            var tagSlug = "test-tag";
+
+            butterClient.MockSuccessfullPostsResponse(page, pageSize, excludeBody, authorSlug, categorySlug, tagSlug);
+
+            var posts = butterClient.ListPosts(page: page, pageSize: pageSize, excludeBody: excludeBody, authorSlug: authorSlug, categorySlug: categorySlug, tagSlug: tagSlug);
             Assert.IsNotNull(posts);
             CollectionAssert.IsNotEmpty(posts.Data);
+
+            var post = posts.Data.First();
+            Assert.AreEqual(PostsMocks.Post.Slug, post.Slug);
+            Assert.AreEqual(PostsMocks.Post.Title, post.Title);
         }
 
         [Test]
         public void ListPosts_NoResults_ShouldReturnNull()
         {   
-            var posts = butterClient.ListPosts(page: 2);
-            Assert.IsNull(posts);
+            var page = 2;
+
+            butterClient.MockSuccessfullEmptyPostsResponse(page);
+
+            var posts = butterClient.ListPosts(page: page);
+            Assert.AreEqual(posts.Meta.Count, 0);
+            CollectionAssert.IsEmpty(posts.Data);
         }
 
         [Test]
         public async Task ListPostsAsync_ShouldReturnPosts()
         {   
+            butterClient.MockSuccessfullPostsResponse();
+
             var posts = await butterClient.ListPostsAsync();
             Assert.IsNotNull(posts);
             CollectionAssert.IsNotEmpty(posts.Data);
+
+            var post = posts.Data.First();
+            Assert.AreEqual(PostsMocks.Post.Slug, post.Slug); 
+            Assert.AreEqual(PostsMocks.Post.Title, post.Title); 
         }
 
         [Test]
         public async Task ListPostsAsync_FullParams_ShouldReturnPosts()
         {   
-            var posts = await butterClient.ListPostsAsync(page: 1, pageSize: 2, excludeBody: true, authorSlug: "api-test", categorySlug: "test-category", tagSlug: "test-tag");
+            var page = 1;
+            var pageSize = 2;
+            var excludeBody = true;
+            var authorSlug = "test-author";
+            var categorySlug = "test-category";
+            var tagSlug = "test-tag";
+
+            butterClient.MockSuccessfullPostsResponse(page, pageSize, excludeBody, authorSlug, categorySlug, tagSlug);
+
+            var posts = await butterClient.ListPostsAsync(page: page, pageSize: pageSize, excludeBody: excludeBody, authorSlug: authorSlug, categorySlug: categorySlug, tagSlug: tagSlug);
             Assert.IsNotNull(posts);
             CollectionAssert.IsNotEmpty(posts.Data);
+
+            var post = posts.Data.First();
+            Assert.AreEqual(PostsMocks.Post.Slug, post.Slug); 
+            Assert.AreEqual(PostsMocks.Post.Title, post.Title); 
         }
 
         [Test]
         public async Task ListPostsAsync_NoResults_ShouldReturnNull()
         {   
-            var posts = await butterClient.ListPostsAsync(page: 2, authorSlug: "not-an-author");
-            Assert.IsNull(posts);
+            var page = 2;
+            var authorSlug = "not-an-author";
+
+            butterClient.MockSuccessfullEmptyPostsResponse(page, authorSlug);
+
+            var posts = await butterClient.ListPostsAsync(page: page, authorSlug: authorSlug);
+
+            Assert.AreEqual(posts.Meta.Count, 0);
+            CollectionAssert.IsEmpty(posts.Data);
         }
     }
 }
---
+++ ButterCMS.Tests/Mocks/PostsMocks.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using ButterCMS.Models;
+
+namespace ButterCMS.Tests
+{
+    public static class PostsMocks
+    {
+        public static Post Post = new Post()
+        {
+            Slug = "post",
+            Title = "Post",
+            Body = "<p>Post body</p>",
+            Author = new Author() { Slug = "author" },
+            Categories = new[] { new Category() { Slug = "category" } },
+            Tags = new[] { new Tag() { Slug = "tag" } },
+        };
+        
+        public static PostsResponse PostsResponse = new PostsResponse()
+        {
+            Data = new[] 
+            { 
+                Post, 
+                new Post() { Slug = "post-2" }
+            },
+            Meta = new PostsMeta()
+            {
+                Count = 2,
+                NextPage = 2,
+                PreviousPage = null,
+            }
+        };
+
+        public static string AppendParamsToListPostsUrl(int? page = null, int? pageSize = null, bool? excludeBody = null, string authorSlug = null, string categorySlug = null, string tagSlug = null)
+        {
+            var url = $"https://api.buttercms.com/v2/posts/?auth_token={ButterCMSClientWithMockedHttp.MockedApiKey}";
+
+            if (page.HasValue && page.Value > 1)
+            {
+                url += $"&page={page.Value}";
+            }
+
+            if (pageSize.HasValue)
+            {
+                url += $"&page_size={pageSize.Value}";
+            }
+
+            if (excludeBody.HasValue)
+            {
+                url += $"&exclude_body={excludeBody.Value}";
+            }
+
+            if (!string.IsNullOrEmpty(authorSlug))
+            {
+                url += $"&author_slug={authorSlug}";
+            }
+
+            if (!string.IsNullOrEmpty(categorySlug))
+            {
+                url += $"&category_slug={categorySlug}";
+            }
+
+            if (!string.IsNullOrEmpty(tagSlug))
+            {
+                url += $"&tag_slug={tagSlug}";
+            }
+
+            Console.WriteLine(url);
+
+            return url;
+        }
+
+        public static void MockSuccessfullPostsResponse(this ButterCMSClientWithMockedHttp butterClient, int? page = null, int? pageSize = null, bool? excludeBody = null, string authorSlug = null, string categorySlug = null, string tagSlug = null)
+        {
+            butterClient.MockSuccessfullJSONResponse(AppendParamsToListPostsUrl(page, pageSize, excludeBody, authorSlug, categorySlug, tagSlug), PostsResponse);
+        }
+
+        public static void MockSuccessfullEmptyPostsResponse(this ButterCMSClientWithMockedHttp butterClient, int? page = null, string authorSlug = null)
+        {
+            butterClient.MockSuccessfullJSONResponse(AppendParamsToListPostsUrl(page, authorSlug: authorSlug), new PostsResponse()
+            {
+                Data = new Post[] { },
+                Meta = new PostsMeta()
+                {
+                    Count = 0,
+                    NextPage = null,
+                    PreviousPage = null,
+                }
+            });
+        }
+    }
+}
