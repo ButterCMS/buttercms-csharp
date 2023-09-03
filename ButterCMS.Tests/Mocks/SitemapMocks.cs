e1c6778b278cd46353a9523b1070aa1a1bd111a2
Vlada Nevyhosteny <vladimir.nevyhosteny@applifting.cz>
2023-09-03 21:26:13 +0200

    fix: Sitemap tests

---
+++ ButterCMS.Tests/Mocks/SitemapMocks.cs
@@ -0,0 +1,19 @@
+﻿using ButterCMS.Models;
+
+namespace ButterCMS.Tests
+{
+    public static class SitemapMocks
+    {
+        public static string Sitemap = @"<?xml version=""1.0"" encoding=""UTF-8""?><Hello>World</Hello>";
+
+        public static XmlAPIResponse SitemapResponse = new XmlAPIResponse()
+        {
+            Data = Sitemap,
+        };
+
+        public static void MockSuccessfullSitemapResponse(this ButterCMSClientWithMockedHttp butterClient)
+        {
+            butterClient.MockSuccessfullJSONResponse($"https://api.buttercms.com/v2/feeds/sitemap/?auth_token={ButterCMSClientWithMockedHttp.MockedApiKey}", SitemapResponse);
+        }
+    }
+}
--- ButterCMS.Tests/SitemapTests.cs
+++ ButterCMS.Tests/SitemapTests.cs
@@ -7,16 +7,19 @@ namespace ButterCMS.Tests
     [Category("Sitemap")]
     public class SitemapTests
     {
-        private ButterCMSClient butterClient;
-        [OneTimeSetUp]
+        private ButterCMSClientWithMockedHttp butterClient;
+
+        [SetUp]
         public void SetUp()
         {
-            butterClient = Common.SetUpButterClient();
+            butterClient = Common.SetUpMockedButterClient();
         }
 
         [Test]
         public void GetSitemapTest_ShouldReturnXMLDocument()
         {
+            butterClient.MockSuccessfullSitemapResponse();
+
             var xml = butterClient.GetSitemap();
             Assert.IsNotNull(xml);
         }
@@ -24,6 +27,8 @@ namespace ButterCMS.Tests
         [Test]
         public async Task GetSitemapAsyncTest_ShouldReturnXMLDocument()
         {
+            butterClient.MockSuccessfullSitemapResponse();
+
             var xml = await butterClient.GetSitemapAsync();
             Assert.IsNotNull(xml);
         }
