182181a46aa0c463af5ff72528c5bc4255e50390
Vlada Nevyhosteny <vladimir.nevyhosteny@applifting.cz>
2023-09-03 13:24:08 +0200

    fix: ListCategories tests

--- ButterCMS.Tests/ListCategoriesTests.cs
+++ ButterCMS.Tests/ListCategoriesTests.cs
@@ -9,44 +9,70 @@ namespace ButterCMS.Tests
     [Category("ListCategories")]
     public class ListCategoriesTests
     {
-        private ButterCMSClient butterClient;
+        private ButterCMSClientWithMockedHttp butterClient;
 
         [OneTimeSetUp]
         public void SetUp()
         {
-            butterClient = Common.SetUpButterClient();
+            butterClient = Common.SetUpMockedButterClient();
         }
 
         [Test]
         public void ListCategories_ShouldReturnListOfCategoriesWithoutPosts()
         {
+            butterClient.MockSuccessfullCategoriesResponse();
+
             var response = butterClient.ListCategories();
             Assert.IsNotNull(response);
+
+            var category = response.FirstOrDefault();
+            Assert.AreEqual(CategoriesMocks.Category.Name, category.Name);
+            Assert.AreEqual(CategoriesMocks.Category.Slug, category.Slug);
             Assert.IsNull(response.FirstOrDefault().RecentPosts);
         }
 
         [Test]
         public async Task ListCategoriesAsync_ShouldReturnListOfCategoriesWithoutPosts()
         {
+            butterClient.MockSuccessfullCategoriesResponse();
+
             var response = await butterClient.ListCategoriesAsync();
             Assert.IsNotNull(response);
+
+            var category = response.FirstOrDefault();
+            Assert.AreEqual(CategoriesMocks.Category.Name, category.Name);
+            Assert.AreEqual(CategoriesMocks.Category.Slug, category.Slug);
             Assert.IsNull(response.FirstOrDefault().RecentPosts);
         }
 
         [Test]
         public void ListCategories_ShouldReturnListOfCategoriesWithPosts()
         {
+            butterClient.MockSuccessfullCategoriesResponseWithPosts();
+
             var response = butterClient.ListCategories(true);
             Assert.IsNotNull(response);
+
+            var category = response.FirstOrDefault();
+            Assert.AreEqual(CategoriesMocks.CategoryWithPosts.Name, category.Name);
+            Assert.AreEqual(CategoriesMocks.CategoryWithPosts.Slug, category.Slug);
             Assert.IsNotEmpty(response.FirstOrDefault().RecentPosts);
+            Assert.AreEqual(CategoriesMocks.CategoryWithPosts.RecentPosts.FirstOrDefault().Slug, category.RecentPosts.FirstOrDefault().Slug);
         }
 
         [Test]
         public async Task ListCategoriesAsync_ShouldReturnListOfCategoriesWithPosts()
         {
+            butterClient.MockSuccessfullCategoriesResponseWithPosts();
+
             var response = await butterClient.ListCategoriesAsync(true);
             Assert.IsNotNull(response);
+
+            var category = response.FirstOrDefault();
+            Assert.AreEqual(CategoriesMocks.CategoryWithPosts.Name, category.Name);
+            Assert.AreEqual(CategoriesMocks.CategoryWithPosts.Slug, category.Slug);
             Assert.IsNotEmpty(response.FirstOrDefault().RecentPosts);
+            Assert.AreEqual(CategoriesMocks.CategoryWithPosts.RecentPosts.FirstOrDefault().Slug, category.RecentPosts.FirstOrDefault().Slug);
         }
     }
 }
---
+++ ButterCMS.Tests/Mocks/CategoriesMocks.cs
@@ -0,0 +1,40 @@
+﻿using ButterCMS.Models;
+
+namespace ButterCMS.Tests
+{
+    public static class CategoriesMocks
+    {
+        public static Category Category = new Category()
+        {
+            Name = "Category",
+            Slug = "category",
+        };
+
+        public static Category CategoryWithPosts = new Category()
+        {
+            Name = "Category",
+            Slug = "category",
+            RecentPosts = new[] { new Post() { Slug = "post-1" }, new Post() { Slug = "post-2" } }
+        };
+
+        public static CategoriesResponse CategoriesResponse = new CategoriesResponse()
+        {
+            Data = new[] { Category },
+        };
+
+        public static CategoriesResponse CategoriesResponseWithPosts = new CategoriesResponse()
+        {
+            Data = new[] { CategoryWithPosts },
+        };
+
+        public static void MockSuccessfullCategoriesResponse(this ButterCMSClientWithMockedHttp butterClient)
+        {
+            butterClient.MockSuccessfullJSONResponse($"https://api.buttercms.com/v2/categories/?auth_token={ButterCMSClientWithMockedHttp.MockedApiKey}", CategoriesResponse);
+        }
+
+        public static void MockSuccessfullCategoriesResponseWithPosts(this ButterCMSClientWithMockedHttp butterClient)
+        {
+            butterClient.MockSuccessfullJSONResponse($"https://api.buttercms.com/v2/categories/?auth_token={ButterCMSClientWithMockedHttp.MockedApiKey}&include=recent_posts", CategoriesResponseWithPosts);
+        }
+    }
+}
